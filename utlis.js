47282aa9a30a16e96a67029b1163653a6dddd4fb
Dheeraj agrahari <93854270+agraharidheeraj@users.noreply.github.com>
2024-09-12 11:16:19 +0530

    Update utlis.js

--- utlis.js
+++ utlis.js
@@ -154,5 +154,60 @@ export const FriendCardSkeleton = () => (
         break;
 
 
+//right side sponser
+
+  <p>Privacy · Terms · Advertising ·</p>
+            <p>· Meta © 2024</p>
+
+
+
+
+ const sponsors = [
+    {
+      name: "Netflix",
+      description: "Watch the latest trending movies and series.",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3qmBuBERRMhoFTvvNUWw7Kr9iicoxC4c8ZQ&s",
+      website: "https://www.netflix.com",
+    },
+    {
+      name: "Instagram",
+      description: "Explore the latest features and connect with friends.",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0NoNt4ECTrCIzRA6PhvyyPThBY9OUEW0-ng&s",
+      website: "https://www.instagram.com",
+    },
+    {
+      name: "Spotify",
+      description: "Stream your favorite music anytime, anywhere.",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShpvQJUXehm_yT1kr2WSATHaDRF88_JjWHcQ&s",
+      website: "https://www.spotify.com",
+    },
+    {
+      name: "Amazon",
+      description: "Shop for everything you need with fast delivery.",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaBLSbp3rFpIZ0kzoreJLN7uZqkJz0h6-RQQ&s",
+      website: "https://www.amazon.com",
+    },
+    {
+      name: "Apple",
+      description: "Discover innovative products and services.",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0qsC4qgsmGTJ9HnNYatRyi7GyJ7GlRMujlw&s",
+      website: "https://www.apple.com",
+    },
+  ];
+
+
+//footer
+  <p>Privacy · Terms · Advertising ·</p>
+    <p>· Meta © 2024</p>
+
+
+
+//npm install
+
+npm install emoji-picker-react  date-fns  react-hot-toast  zustand   axios  yup @hookform/resolvers react-hook-form   next-themes   framer-motion  lucide-react 
+
+
+
+
 
 
